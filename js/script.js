602557eba46664205d8bddd99b91f29fa55c8720
olailz <olailz10.3@gmail.com>
2025-06-05 16:18:01 -0600

    Primera prueba con index, style, reponsive y script de mi portafolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburguesa = document.querySelector(".hamburguesa");
+const navMenu = document.querySelector("nav ul");
+
+hamburguesa.addEventListener("click",() =>{
+    navMenu.classList.toggle("verMenu");
+});
+
