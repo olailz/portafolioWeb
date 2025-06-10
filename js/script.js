19a7d93216c3a04c8c6fa9d3f0dedd38148836e6
olailz <olailz10.3@gmail.com>
2025-06-09 23:50:28 -0600

    se arreglo el problema de la nav hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    const hamburguesa = document.getElementById("hamburguesa");
-    const menu = document.getElementById("menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburguesa = document.getElementById("hamburguesa");
+  const menu = document.getElementById("menu");
 
-    hamburguesa.addEventListener("click", function () {
-      menu.classList.toggle("verMenu");
-    });
+  hamburguesa.addEventListener("click", function () {
+    menu.classList.toggle("verMenu");
   });
+});
